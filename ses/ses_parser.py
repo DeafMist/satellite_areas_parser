a41b8785bd77193803385861be51de7ddbbfe261
DeafMist <romapr70@gmail.com>
2023-05-20 23:38:45 +0300

    Cleaned up the repository

--- ses/ses_parser.py
+++ ses/ses_parser.py
@@ -2,12 +2,13 @@ import requests
 
 from kml_generator import kml_generator
 
+
 def ses_parser():
     satel_numbers = [309, 311, 312, 322, 324, 325, 326, 329, 330, 332, 337, 339, 343, 344, 345, 347, 351, 352, 353, 355,
                      356, 357, 361, 363, 365, 366, 369, 371, 372, 374, 408, 409, 410, 411]
     kml_data = []
 
-    for i in satel_numbers:    
+    for i in satel_numbers:
         response = requests.get(f'https://www.ses.com/satellite/{i}/footprints?_format=hal_json')
         satel = response.json()
         for footprint in satel['footprints']:
@@ -20,7 +21,7 @@ def ses_parser():
 
             temp_data['multypolygon'] = []
             for k in footprint['geo_json']['features'][0]['geometry']['coordinates']:
-                poly = '' 
+                poly = ''
                 for l in k[0]:
                     poly += ' ' + str(l[0]) + ',' + str(l[1])
                 temp_data['multypolygon'].append(poly)
